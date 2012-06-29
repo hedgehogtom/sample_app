f62e36d6e992b375d9abb67c79ec70aea9ffe751
hedgehogtom <twionzek@gmail.com>
2012-06-28 22:37:08 -0700

    Finished static pages (ch 3)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def help
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About Us"
   end
 end
