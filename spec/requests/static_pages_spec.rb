992149b53c544ce40699663291202a5bfec296b2
hedgehogtom <twionzek@gmail.com>
2012-07-25 15:16:54 -0700

    Added full_title utility to spec/support; updated Ruby 1.8 hashes to Ruby 1.9 in spec requests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,33 +8,33 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', :text => 'Sample App') }
-    it { should have_selector('title', :text => "#{base_title}") }
-    it { should_not have_selector('title', :text => "#{base_title} | Home") }
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: full_title('Home')) }
   end
   
   describe "Help page" do
     before { visit help_path }
     
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "#{base_title} | Help") }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
   
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('h1', text: 'About Us') }
     it { should have_selector('title', 
-      :text => "#{base_title} | About Us") }
+      text: full_title('About Us')) }
   end
 
   # Tests for a Contact page
   describe "Contact page" do
     before { visit contact_path }
 
+    it { should have_selector('h1', text: 'Contact Us') }
     it { should have_selector('title', 
-      :text => "#{base_title} | Contact Us") }
-    it { should have_selector('h1', :text => 'Contact Us') }
+      text: full_title('Contact Us')) }
   end
 
 end
