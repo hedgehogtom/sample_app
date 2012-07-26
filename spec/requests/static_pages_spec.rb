549477ce4bae56ddf18138a0a8abd63b9c9c5c47
hedgehogtom <twionzek@gmail.com>
2012-07-25 17:08:08 -0700

    Completed Ch 5 exercises: Added shared examples in spec file; eliminated full_title utilities helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,37 +3,44 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }  # 'page' is the subject of all calls to 'should'
-   
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { '' }
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: full_title('Home')) }
   end
   
   describe "Help page" do
     before { visit help_path }
-    
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
   
   describe "About page" do
     before { visit about_path }
+    let(:heading)     { 'About Us' }
+    let(:page_title)  { 'About Us' }
 
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', 
-      text: full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   # Tests for a Contact page
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)     { 'Contact Us' }
+    let(:page_title)  { 'Contact Us' }
 
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', 
-      text: full_title('Contact Us')) }
+    it_should_behave_like "all static pages"
   end
 
   it "should have the right links on the layout" do
@@ -45,8 +52,11 @@ describe "Static pages" do
     click_link "Contact"
     should have_selector 'title', text: full_title('Contact Us')
     click_link "Home"
+    should have_selector 'title', text: full_title('')
     click_link "Sign up now!"
     should have_selector 'title', text: full_title('Sign Up')
+    click_link "sample app"
+    should have_selector 'title', text: full_title('')
   end
 end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,7 @@
 # A file for RSpec utilities
 
 # Essentially a duplicate of app/helpers/application_helpers.rb, for now
+=begin
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
@@ -9,3 +10,7 @@ def full_title(page_title)
 		"#{base_title} | #{page_title}"
 	end
 end
+=end
+
+# full_title code now replaced with this include
+include ApplicationHelper
